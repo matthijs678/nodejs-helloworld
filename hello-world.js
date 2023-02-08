c8ceda5f8853c1921b0499333e032b1e6321e1d2
matthijs678 <40187960@roctilburg.nl>
2023-02-08 13:56:33 +0100

    init

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+const log = require('pretty-log');
+const http = require('node:http');
+
+const port = 80;
+const hostname = "http://127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  log.success(`Server running at http://${hostname}:${port}/`);
+});
